98dac9527b859d07b09daa0f6ab9101981ef4a67
julio-crypt0 <hernandezjulio58@ymail.com>
2019-10-04 13:15:28 -0700

    first media query done

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 'use strict';
 function hamburger() {
     let x = document.getElementsByClassName('menu');
-    console.log(x);
-    if(x.style.display ==='none'){
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
+    for(let i=0; i < x.length; i++){
+        if(x[i].style.display ==='none'){
+            x[i].style.display = 'block';
+        } else {
+            x[i].style.display = 'none';
+        }
+    };
+}
+function checkScreenWidth() {
+    if($(window).width() >= 1000) {
+        disableHamburger();
     }
 }
-$(hamburger);
+
+function disableHamburger() {
+    let listContainer = $('.list-container')[0];
+    console.log(listContainer);
+    listContainer.removeAttr(onclick);
+}
+
+$(checkScreenWidth);
